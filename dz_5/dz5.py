1cbfa4e5a285cc423505be31ce3797f8107ac05a
Xantarres <144926666+Xantarres@users.noreply.github.com>
2024-10-28 18:43:23 +0300

    Delete dz_5 directory

--- dz_5/dz5.py
+++
@@ -1,8 +0,0 @@
-text = "Пример текста для подсчета гласных букв"
-vowels = "аеияо"
-count = 0
-for i in text:
-    if i in vowels:
-        count += 1
-        print("Количество гласных букв:",count)
-
