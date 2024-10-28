ba1210314e0d85fdc554aca0afd8d747c6de34dc
Xantarres <144926666+Xantarres@users.noreply.github.com>
2024-10-28 18:42:58 +0300

    Delete dz_4 directory

--- dz_4/dz4.py
+++
@@ -1,9 +0,0 @@
-N = int(input())
-k = int(input()) - 1
-people = list(range(1,N+1))
-while len(people) !=1:
-    index = k%len(people)
-    k = index
-    del people[k]
-    k += 2
-    print(people)
